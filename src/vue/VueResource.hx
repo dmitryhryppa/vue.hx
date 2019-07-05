f20b79dfd33a101fa22e5b606e4695914c58477d
Dmitry Hryppa <dmitry.hryppa@gmail.com>
2019-07-05 17:05:18 +0300

    Fix haxe 4 warnings

--- src/vue/VueResource.hx
+++ src/vue/VueResource.hx
@@ -1,9 +1,11 @@
 package vue;
 
+import js.lib.Promise;
+
 @:native("Vue.http")
 extern class VueResource {
-	public static function get<T>(url:String, ?options:{}):js.Promise<Result<T>>;
-	public static function post<T>(url:String, ?body:{}, ?options:{}):js.Promise<Result<T>>;
+	public static function get<T>(url:String, ?options:{}):Promise<Result<T>>;
+	public static function post<T>(url:String, ?body:{}, ?options:{}):Promise<Result<T>>;
 }
 
 typedef Result<T> = {
